483da0b954e6289f0b9db33f27334ca8117d337d
Neolin <joussonantonin@gmail.com>
2020-03-28 17:16:31 +0100

    Add i18n support

---
+++ lambda/custom/data/data.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Resolving gettext as _ for module loading.
+from gettext import gettext as _
+
+SKILL_NAME = "Mon trajet quotidien"
+
+WELCOME = _("Bienvenue dans mon trajet quotidien!")
+HELP = _("Demandez moi quand est-ce que passe le prochain tram.")
+STOP = _("A la prochaine!")
+GENERIC_REPROMPT = _("Comment puis-je vous aider?")
