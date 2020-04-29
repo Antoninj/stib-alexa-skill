ade9c7881323a086a5725ef433e9c76bb81afa0c
Neolin <joussonantonin@gmail.com>
2020-04-29 17:59:39 +0200

    Add copyright

--- lambda/custom/core/handlers/amazon/intent_reflector_handler.py
+++ lambda/custom/core/handlers/amazon/intent_reflector_handler.py
@@ -1,3 +1,17 @@
+#   Copyright 2020 Antonin Jousson
+#
+#  Licensed under the Apache License, Version 2.0 (the "License").
+#  You may not use this file except in compliance with the License.
+#  A copy of the License is located at
+#
+#  http://aws.amazon.com/apache2.0/
+#
+#  or in the "license" file accompanying this file. This file is
+#  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+#  OF ANY KIND, either express or implied. See the License for the
+#  specific language governing permissions and limitations under the
+#  License.
+
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_core.utils import is_request_type, get_intent_name
